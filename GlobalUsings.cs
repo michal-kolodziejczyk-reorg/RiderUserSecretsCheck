042ecc41ced00889e508621f2da79b06f42194f4
cybersan <cybersan@gmail.com>
2024-06-07 10:10:00 +0200

    Ready for checking OS vs user secrets

---
+++ GlobalUsings.cs
@@ -0,0 +1,6 @@
+// Global using directives
+
+global using System.Reflection;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.DependencyInjection;
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// this is a project to check how OS reads user secrets
+// 1. Run the project. It should fail due to the fact that AddUserSecrets has optional argument set to false
+// 2. Change AddUserSecrets optional argument to true
+// 3. Run the project. It should read appsettings.json file and display "appsettings" on Console
+// 4. Add user secrets to the project
+// 5. Put body of appsettings.json into user secrets, change value of "Source" to "user secrets"
+// 6. Run the project. Now user secrets should override appsettings.json and program displays "user secrets"
+
+var builder = Host.CreateEmptyApplicationBuilder(new HostApplicationBuilderSettings());
+builder.Services.AddOptions();
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddUserSecrets(Assembly.GetExecutingAssembly(), optional: false);
+
+var host = builder.Build();
+var configuration = host.Services.GetRequiredService<IConfiguration>();
+Console.WriteLine(configuration.GetValue<string>("Source"));
